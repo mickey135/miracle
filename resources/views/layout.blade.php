dc280ae1e0127331838ccd85ee91b273516c622b
mickey135 <mickey135@foxmail.com>
2017-09-02 15:31:30 +0800

    添加系统消息推送功能

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,7 +18,13 @@
 		  s.parentNode.insertBefore(hm, s);
 		})();
 	</script>
-
+	<!-- 消息弹窗代码 -->
+	<style type="text/css">
+		#winpop { width:200px; height:0px; position:absolute; right:0; bottom:0; border:1px solid #999999; margin:0; padding:1px; overflow:hidden; display:none; background:#FFFFFF}
+		#winpop #tanchuang_title { width:100%; height:20px; line-height:20px; background:#FFCC00; font-weight:bold; text-align:center; font-size:12px;}
+		#winpop #tanchuang_msg { width:100%; height:80px; line-height:80px; font-weight:bold; font-size:12px; color:#FF0000; text-decoration:underline; text-align:center}
+		#tanchuang_close { position:absolute; right:4px; top:-1px; color:#FFFFFF; cursor:pointer}
+	</style>
 </head>
 <body>
 	<!-- 左边栏 -->
@@ -64,5 +70,57 @@
 @section('right')
 
 @show
+<!-- 消息弹窗 -->
+<div id="winpop">
+	<div id="tanchuang_title">系统消息<span id="tanchuang_close" onclick="tips_pop()">X</span></div>
+	<div id="tanchuang_msg"></div>
+</div>
 </body>
+<script type="text/javascript">
+	var tanchuang_msg = document.getElementById('tanchuang_msg');		
+	var ws = new WebSocket('ws://47.94.147.221:9501');
+	ws.onopen = function(){
+
+	}
+	ws.onmessage = function(ev){
+		tanchuang_msg.innerHTML += ev.data;
+		tips_pop();
+	}
+	function tips_pop(){
+	  var MsgPop=document.getElementById("winpop");//获取窗口这个对象,即ID为winpop的对象
+	  var popH=parseInt(MsgPop.style.height);//用parseInt将对象的高度转化为数字,以方便下面比较
+	   if (popH==0){         //如果窗口的高度是0
+	   MsgPop.style.display="block";//那么将隐藏的窗口显示出来
+	  show=setInterval("changeH('up')",2);//开始以每0.002秒调用函数changeH("up"),即每0.002秒向上移动一次
+	   }
+	  else {         //否则
+	   hide=setInterval("changeH('down')",2);//开始以每0.002秒调用函数changeH("down"),即每0.002秒向下移动一次
+	  }
+	}
+	function changeH(str) {
+		 var MsgPop=document.getElementById("winpop");
+		 var popH=parseInt(MsgPop.style.height);
+		 if(str=="up"){     //如果这个参数是UP
+		  if (popH<=100){    //如果转化为数值的高度小于等于100
+		  MsgPop.style.height=(popH+4).toString()+"px";//高度增加4个象素
+		  }
+		  else{  
+		  clearInterval(show);//否则就取消这个函数调用,意思就是如果高度超过100象度了,就不再增长了
+		  }
+		 }
+		 if(str=="down"){ 
+		  if (popH>=4){       //如果这个参数是down
+		  MsgPop.style.height=(popH-4).toString()+"px";//那么窗口的高度减少4个象素
+		  }
+		  else{        //否则
+		  clearInterval(hide);    //否则就取消这个函数调用,意思就是如果高度小于4个象度的时候,就不再减了
+		  MsgPop.style.display="none";  //因为窗口有边框,所以还是可以看见1~2象素没缩进去,这时候就把DIV隐藏掉
+		  }
+		 }
+	}
+	window.onload=function(){    //加载
+	document.getElementById('winpop').style.height='0px';//我不知道为什么要初始化这个高度,CSS里不是已经初始化了吗,知道的告诉我一下
+	setTimeout("tips_pop()",800);     //3秒后调用tips_pop()这个函数
+	}
+</script>
 </html>
